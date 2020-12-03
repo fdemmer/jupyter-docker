22036cbe1ae8f0800f741b0ae3db19db730c2544
Anton Pirker <anton@ignaz.at>
2020-12-03 14:16:33 +0100

    Do not open browser

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -1,3 +1,4 @@
 c.NotebookApp.allow_remote_access=True
 c.NotebookApp.allow_password_change=False
+c.NotebookApp.open_browser = False
 c.NotebookApp.password = u'argon2:$argon2id$v=19$m=10240,t=10,p=8$jdIFttxBdO83V3kEI1D/5A$9JxzfZkGLFx2lIIUgSZJJg'
